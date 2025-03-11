eec2c0e0e559b393117cf06c9fed4fd3ef11886f
KChan-ka <thuntor@hotmail.com>
2025-03-11 15:55:00 +0000

    implemented comment_count to /api/articles/:article_id

--- model/model.js
+++ model/model.js
@@ -17,7 +17,21 @@ exports.selectTopics = () => {
 
 exports.selectArticleById = (articleId) => {
 
-    const queryString = `SELECT * FROM articles WHERE article_id = $1`
+    const queryString = `
+    SELECT 
+        a.article_id,
+        a.title,
+        a.topic,
+        a.author,
+        a.body,
+        a.created_at,
+        a.votes,
+        a.article_img_url,
+        count(c.comment_id) AS comment_count
+    FROM articles a
+        JOIN comments c ON a.article_id = c.article_id
+    WHERE a.article_id = $1
+    GROUP BY a.article_id`
 
     return db
         .query(queryString, [articleId])
